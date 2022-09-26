95a87e99e324fd6337a930672a22430cecdc926d
temmy76 <temmy.mahesa.tif420@polban.ac.id>
2022-09-26 20:10:35 +0700

    implementing brightness and bitwise to the image

--- main.py
+++ main.py
@@ -30,11 +30,86 @@ def getRGBfromImage(filename):
 	return r, g, b
 
 def brightness_add(image, value):
-    image = np.asarray(image).astype('uint16')
-    image = image+value
+	image = np.asarray(image).astype('uint16')
+	image = image+value
+	image = np.clip(image, 0, 255)
+	new_image = image.astype('uint8')
+
+	return new_image
+
+def brightness_addcv(image, value):
+    new_image = cv.add(image, value)
+    return new_image
+
+def brightness_subtraction(image, value):
+	image = image.astype('uint16')
+	image = image-value
+	image = np.clip(image, 0, 255)
+	new_image = image.astype('uint8')
+	return new_image
+
+
+def brightness_subtractioncv(image, value):
+    new_image = cv.subtract(image, value)
+    new_image = np.clip(new_image, 0, 255)
+    return new_image
+
+def brightness_multiplication(image, value):
+    image = image.astype('uint16')
+    image = image*value
     image = np.clip(image, 0, 255)
     new_image = image.astype('uint8')
     return new_image
+
+def brightness_multiplicationcv(image, value):
+    new_image = cv.multiply(image, value)
+    new_image= np.clip(new_image, 0, 255)
+    return new_image
+
+
+def brightness_divide(image, value):
+    image = image.astype('uint16')
+    image = image/value
+    image = np.clip(image, 0, 255)
+    new_image = image.astype('uint8')
+    return new_image
+
+def brightness_dividecv(image, value):
+	new_image = cv.divide(image, value)
+	new_image = np.clip(new_image, 0, 255)
+	return new_image
+
+def bitwise_and(image1, image2):
+    bit_and = cv.bitwise_and(image1, image2)
+    return bit_and
+
+def bitwise_and_nocv(image1, image2):
+	bit_and = image1 & image2
+	return bit_and
+
+def bitwise_or(image1, image2):
+    bit_or = cv.bitwise_or(image1, image2)
+    return bit_or
+
+def bitwise_or_nocv(image1, image2):
+	bit_or = image1 | image2
+	return bit_or
+
+def bitwise_not(image):
+    bit_not = cv.bitwise_not(image)
+    return bit_not
+
+def bitwise_not_nocv(image):
+	bit_not = ~image
+	return bit_not
+
+def bitwise_xor(image1, image2):
+    bit_xor = cv.bitwise_xor(image1, image2)
+    return bit_xor
+
+def bitwise_xor_nocv(image1, image2):
+	bit_xor = image1 ^ image2
+	return bit_xor
 
 @app.route('/')
 def upload_form():
@@ -93,7 +168,6 @@ def grayscaleImage(filename):
 	cv.imwrite("static/uploads/{}".format(filename), gray)
 	return redirect(url_for('static', filename='uploads/grayscale/' + filename), code=301)
 
-# create route that render brightness.html template
 @app.route('/brightness')
 def brightness():
 	return render_template('brightness.html')
@@ -123,10 +197,13 @@ def upload_brightness():
 		flash('Allowed image types are -> png, jpg, jpeg, gif')
 		return redirect(request.url)
 
-# create route for change the brightness of image
-@app.route('/brightness/<filename>', methods=['POST'])
+@app.route('/brightness/<filename>', methods=['GET', 'POST'])
 def brightnessImage(filename):
 	r, g, b = getRGBfromImage(filename)
+
+	operation = request.form['operation']
+	method = request.form['method']
+
 	# get the dimensions of the image
 	image = cv.imread("static/uploads/{}".format(filename))
 	reso = image.shape
@@ -134,14 +211,37 @@ def brightnessImage(filename):
 	length = reso[0]
 	# get the value of brightness
 	brightness = int(request.form['brightness'])
-	# change the brightness of image with the value of brightness using openCV
-	new_image = brightness_add(image, brightness)
+	# check if the method is cv or not
+	if method == 'opencv':
+		print('menggunakan cv')
+		if operation == 'add':
+			new_image = brightness_addcv(image, brightness)
+		elif operation == 'substract':
+			new_image = brightness_subtractioncv(image, brightness)
+		elif operation == 'multiply':
+			new_image = brightness_multiplicationcv(image, brightness)
+		elif operation == 'divide':
+			new_image = brightness_dividecv(image, brightness)
+		else:
+			new_image = image
+	else:
+		print('tidak menggunakan cv')
+		if operation == 'add':
+			new_image = brightness_add(image, brightness)
+		elif operation == 'substract':
+			new_image = brightness_subtraction(image, brightness)
+		elif operation == 'multiply':
+			new_image = brightness_multiplication(image, brightness)
+		elif operation == 'divide':
+			new_image = brightness_divide(image, brightness)
+		else:
+			new_image = image
+
 	new_filename = "brightness-" + filename
 	# save the image
 	cv.imwrite("static/uploads/{}".format(new_filename), new_image)
 	return render_template('brightness.html', filename=filename, brightness=new_filename)
 
-# create route for displaying brightness image
 @app.route('/brightness/display/<filename>')
 def display_brightness_image(filename):
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
@@ -150,6 +250,83 @@ def display_brightness_image(filename):
 @app.route('/bitwise')
 def bitwise():
 	return render_template('bitwise.html')
+
+@app.route('/bitwise', methods=['POST'])
+def upload_bitwise():
+	if 'image1' not in request.files or 'image2' not in request.files:
+		return redirect(request.url)
+
+	image_1 = request.files['image1'] 
+	image_2 = request.files['image2']
+
+	if image_1.filename == None or image_2.filename == None:
+		return redirect(request.url)
+
+	if image_1 and allowed_file(image_1.filename) and image_2 and allowed_file(image_2.filename):
+		# save the file to the uploads folder
+		if image_1 and image_2:
+			filename1 = secure_filename(image_1.filename)
+			image_1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
+			filename2 = secure_filename(image_2.filename)
+			image_2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
+			# flash message to the user
+			print('Image successfully uploaded and displayed below')
+			# return the template with the filename and original image
+			return render_template('bitwise.html', image1=filename1, image2=filename2)
+	else:
+		print('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+
+@app.route('/bitwise/<filename1>/<filename2>', methods=[ 'GET','POST'])
+def bitwiseImage(filename1, filename2):
+	# get the value of bitwise operation
+	bitwise = request.form['bitwise']
+	operation = request.form['operation']
+	# get the images
+	image1 = cv.imread("static/uploads/{}".format(filename1))
+	image2 = cv.imread("static/uploads/{}".format(filename2))
+
+	#resize the images to the same size
+	image1 = cv.resize(image1, (200,200))
+	image2 = cv.resize(image2, (200,200))
+
+	if operation == 'manual':
+		print("using manual")
+		if bitwise == 'and':
+			bitwise = bitwise_and_nocv(image1, image2)
+		elif bitwise == 'or':
+			bitwise = bitwise_or_nocv(image1, image2)
+		elif bitwise == 'xor':
+			bitwise = bitwise_xor_nocv(image1, image2)
+		elif bitwise == 'not':
+			bitwise = bitwise_not_nocv(image1)
+		else:
+			bitwise = bitwise_not_nocv(image1)
+
+	else:
+		print("using cv")
+		if bitwise == 'and':
+			bitwise = bitwise_and(image1, image2)
+		elif bitwise == 'or':
+			bitwise = bitwise_or(image1, image2)
+		elif bitwise == 'xor':
+			bitwise = bitwise_xor(image1, image2)
+		elif bitwise == 'not':
+			bitwise = bitwise_not(image1)
+		else:
+			bitwise = bitwise_not(image1)
+	# save the image
+	new_filename = "bitwise-" + filename1 + "-" + filename2
+	cv.imwrite("static/uploads/{}".format(new_filename), bitwise)
+	print(new_filename)
+	return render_template('bitwise.html', image1=filename1, image2=filename2, result=new_filename)
+
+
+@app.route('/bitwise/display/<filename>')
+def display_bitwise_image(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
